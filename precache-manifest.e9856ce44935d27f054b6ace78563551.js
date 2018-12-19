08180fd1dd1ea1b5964555fe7d2f770225a8ba8a
LizethPatino <lizeth@outlook.com>
2018-12-19 17:26:45 -0500

    Updates

---
+++ precache-manifest.e9856ce44935d27f054b6ace78563551.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/PrimerProyectoReact/./static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "d710ea66b2213fded1ec",
+    "url": "/PrimerProyectoReact/./static/js/runtime~main.d710ea66.js"
+  },
+  {
+    "revision": "43bcc5c920a5eb2fd8e5",
+    "url": "/PrimerProyectoReact/./static/js/main.43bcc5c9.chunk.js"
+  },
+  {
+    "revision": "563bd48ede45e839e65f",
+    "url": "/PrimerProyectoReact/./static/js/1.563bd48e.chunk.js"
+  },
+  {
+    "revision": "43bcc5c920a5eb2fd8e5",
+    "url": "/PrimerProyectoReact/./static/css/main.0c3a3092.chunk.css"
+  },
+  {
+    "revision": "b92e1f944c0ff59a37295b2d674b0649",
+    "url": "/PrimerProyectoReact/./index.html"
+  }
+];
