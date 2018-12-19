f4f28e2804cbdce998efe20c0d7f3ab364cd7684
LizethPatino <lizeth@outlook.com>
2018-12-19 17:30:42 -0500

    Updates

---
+++ precache-manifest.5e2503808e6564cd677d8ac9dc59b67c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/PrimerProyectoReact/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "646717b6b650a2d37658",
+    "url": "/PrimerProyectoReact/static/js/runtime~main.646717b6.js"
+  },
+  {
+    "revision": "f68b5df5233abd3c5bf1",
+    "url": "/PrimerProyectoReact/static/js/main.f68b5df5.chunk.js"
+  },
+  {
+    "revision": "563bd48ede45e839e65f",
+    "url": "/PrimerProyectoReact/static/js/1.563bd48e.chunk.js"
+  },
+  {
+    "revision": "f68b5df5233abd3c5bf1",
+    "url": "/PrimerProyectoReact/static/css/main.0c3a3092.chunk.css"
+  },
+  {
+    "revision": "1a814dfeb2241b8369444ce9a0193c70",
+    "url": "/PrimerProyectoReact/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/PrimerProyectoReact/./precache-manifest.e9856ce44935d27f054b6ace78563551.js"
+  "/PrimerProyectoReact/precache-manifest.5e2503808e6564cd677d8ac9dc59b67c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/PrimerProyectoReact/./index.html", {
+workbox.routing.registerNavigationRoute("/PrimerProyectoReact/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
