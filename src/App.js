af6741c41518dc2eebe123ff7b96004ccc1bc55f
LizethPatino <lizeth@outlook.com>
2018-12-19 14:59:59 -0500

    adición contador de tareras y de tarjetas de especificacion

--- src/App.js
+++ src/App.js
@@ -1,14 +1,52 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Navigation from './components/Navigation';
+import {toDo} from './toDo.json';
+
+
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state ={
+     toDo
+    }
+  }
   render() {
+  const todos =  this.state.toDo.map((elemento, i ) => {
+      return(
+       
+       <div className="col-md-4">
+          <div className="card mt-4">
+            <div className="card-header">
+                <h3>{elemento.title}</h3>
+                <span className="badge badge-pill badge-danger ml-2">
+                  {elemento.priority}
+                </span>
+            </div>
+            <div className="card-body">
+            <p>{elemento.description}</p>
+            <p><mark>{elemento.responsible}</mark></p>
+            </div>
+          </div>
+       </div>  
+      )
+    } )
     return (
-      <div className="App">  
-      <Navigation titulo="Tasks" />       
-      <img src={logo} className="App-logo" alt="logo" />        
+      <div className="App"> 
+
+       <nav className="navbar navbar-dark bg-dark">
+                <a href="" className="text-white" >Tasks
+                <span className="badge badge-pill badge-light ml-2">
+                    {this.state.toDo.length}
+                </span>
+                </a>
+      </nav>  
+        <div className="container">
+          <div className="row mt-4">
+            {todos}
+          </div>
+        </div>
       </div>
     );
   }
